f0872d0c54f61b987b4f892f0da0048c93238cb3
Israel Teneda <israteneda@gmail.com>
2023-08-13 18:00:21 -0500

    format file

--- sorting.js
+++ sorting.js
@@ -1,14 +1,14 @@
 function sortList(unsortedList) {
-		for (let i = 0; i < unsortedList.length; i++) {
-    	if (unsortedList[i] < unsortedList[i - 1]) {      	
-        const temp = unsortedList[i - 1];
-	      unsortedList[i - 1] = unsortedList[i];
-        unsortedList[i] = temp;
-				i -= 2;
-      }
-    } 
-    
-    return unsortedList;
+  for (let i = 0; i < unsortedList.length; i++) {
+    if (unsortedList[i] < unsortedList[i - 1]) {
+      const temp = unsortedList[i - 1];
+      unsortedList[i - 1] = unsortedList[i];
+      unsortedList[i] = temp;
+      i -= 2;
+    }
+  }
+
+  return unsortedList;
 }
 
 module.exports = sortList;
